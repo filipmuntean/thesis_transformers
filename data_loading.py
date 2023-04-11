0584dd0725abeed564279476f3ce8cabcd809864
filipmuntean <f.muntean@student.vu.nl>
2023-04-11 22:05:31 +0200

    still grinding

--- data_loading.py
+++ data_loading.py
@@ -2,6 +2,10 @@ from data_rnn import sort_reviews
 import data_rnn
 import torch.nn.utils.rnn as rnn_utils
 import torch
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 BATCH_SIZE = 32
 SEQUENCE_LENGTH = 10
@@ -21,7 +25,7 @@ def batch_reviews_by_instance(x_train, batch_size):
 
 def batch_reviews_by_tokens(x_train, seq_len):
     batches = []
-    # for seq in x_train
+    # for seq in x_train:
     for i in range(0, len(x_train)):
         if i % seq_len == 0:
             batch = x_train[i:i+seq_len]
@@ -42,7 +46,7 @@ def batch_reviews_by_instance_and_by_tokens(x_train, i2w, batch_size, seq_len):
         batch_token_count = 0
         
         for seq in batch:
-            seq_words = [i2w[idx] for idx in seq]
+            seq_words = [idx for idx in seq]
             seq_token_count = len(seq_words)
             
             # Add the sequence to the current batch if it doesn't exceed the token count
@@ -55,7 +59,7 @@ def batch_reviews_by_instance_and_by_tokens(x_train, i2w, batch_size, seq_len):
             #     print("First 5 of the batch:\n")
             #     print(batch_words[:5], "\n")
                 batches.append(batch_words)
-                batch_words = [seq_words]
+                batch_words = seq_words
                 batch_token_count = seq_token_count
         
         # # Print the final batch if it's not empty
@@ -65,31 +69,21 @@ def batch_reviews_by_instance_and_by_tokens(x_train, i2w, batch_size, seq_len):
 
 batched_sorted_reviews = batch_reviews_by_instance_and_by_tokens(sorted_reviews, data_rnn.i2w, BATCH_SIZE, SEQUENCE_LENGTH)
 
+# print(batched_sorted_reviews[:5])
 max_len = max(len(review) for review in [batched_sorted_reviews])
 
 def pad_sequence():
     padded_seqs = []
     for review in batched_sorted_reviews:
-        padded_seq = rnn_utils.pad_sequence([torch.tensor(data_rnn.i2w)], 
+        tensor_sequence = torch.tensor(batched_sorted_reviews)
+        padded_seq = rnn_utils.pad_sequence([tensor_sequence], 
                                             batch_first=True, 
                                             padding_value=0, 
-                                            total_length=max_len)
-        # padded_seq = review + [pad_token] * (max_len - len(review))
+                                            total_length = max_len, 
+                                            dtype = torch.long)
+        # padded_seq = review + [batch_token_count] * (max_len - len(review))
         padded_seqs.append(padded_seq)
     return padded_seqs
-
-def pad_batch_reviews(batch_reviews, pad_token):
-    # Get the maximum length of the sequences in the batch
-    max_len = max(len(review) for review in batch_reviews)
-
-    # Pad all sequences to the maximum length with the pad_token
-    padded_batch = []
-    for review in batched_sorted_reviews:
-        
-        padded_batch.append(padded_review)
-
-    return padded_batch
-
 
 padded_sequence = pad_sequence()
 
